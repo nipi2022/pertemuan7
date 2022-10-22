c7797af940826e23468b274a77e5f55a87d62287
NIPI MASPUTRA <113754634+nipi2022@users.noreply.github.com>
2022-10-22 18:53:35 +0700

    Add files via upload

---
+++ app.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class koperasi {
+    
+    private String idAnggota;
+    private String namaAnggota;
+    private String nomorRekening;
+    
+    int pinjam;
+    double saldo,pajak;
+    
+    //method non void
+    
+    public koperasi (String idAnggota, String namaAnggota, String nomorRekening){
+        this.idAnggota = idAnggota;
+        this.namaAnggota = namaAnggota;
+        this.nomorRekening = nomorRekening;
+    }
+
+    koperasi(int saldo, int pinjam, double pajak){
+        this.saldo = saldo;
+        this.pinjam = pinjam;
+        this.pajak = pajak;
+    }
+
+    double hitungSaldo (){
+        return this.saldo = (saldo + (pinjam - (pinjam * pajak)));
+    }
+    
+    //Method Void
+    
+    /**
+     * 
+     */
+    void tampil (){
+         System.out.println ("Id Anggota : "+this.idAnggota);
+        System.out.println ("Nama Anggota : "+this.namaAnggota);
+        System.out.println ("Nomor Rekenng : "+this.nomorRekening);
+        System.out.println ();
+    }
+    
+    void ambilSaldo (){
+        System.out.println ("Saldo anda : "+ saldo);
+        System.out.println ("Jumlah pinjaman : "+this.pinjam);
+        System.out.println ("Bunga pinjaman : "+this.pajak);
+        System.out.println ("Sisa saldo anda sebesar : "+  this.hitungSaldo());
+    }
+}
+
+public class app {
+    public static void main ( String [] args){
+        Scanner sc = new Scanner(System.in);
+        koperasi anggota = new koperasi ("ID-050","Nipi masputra","RK-005");
+        koperasi ambil = new koperasi (300000,30000,0.6);
+        anggota.tampil();
+        ambil.ambilSaldo();
+    }
+}
